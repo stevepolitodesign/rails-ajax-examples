f4317331e0d997a07b785229ad17dc0e7207b216
Steve Polito <info@stevepolitodesign.com>
2019-10-08 16:24:54 -0400

    commit partials, views and controllers

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,50 @@
+class AuthorsController < ApplicationController
+
+    before_action :set_author, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @authors = Author.all
+    end
+
+    def show
+    end
+
+    def new
+        @author = Author.new
+    end
+
+    def create
+        @author = Author.create(author_params)
+        if @author.save
+            redirect_to @author, notice: "#{@author.formatted_name} was successfully saved."
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @author.update(author_params)
+            redirect_to author_path(@author), notice: "#{@author.formatted_name} was successfully updated."
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @author.destroy
+        redirect_to authors_path, notice: "#{@author.formatted_name} was successfully deleted."
+    end
+
+    private 
+
+        def set_author
+            @author = Author.find(params[:id])
+        end
+
+        def author_params
+            params.require(:author).permit(:first_name, :last_name)
+        end
+end
