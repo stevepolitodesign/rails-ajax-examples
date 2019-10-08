f29948219a6b5d894809850e620b6bec4534c00d
Steve Polito <info@stevepolitodesign.com>
2019-10-07 20:56:53 -0400

    write comment specs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
+
+  validates :body, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "creation" do
+    let(:comment) { FactoryBot.create(:comment) }
+    it "can be created" do
+      expect(comment).to be_valid
+    end
+  end
+
+  describe "validations" do
+    let(:comment) { FactoryBot.create(:comment) }
+    it "must have a body" do
+      comment.body = nil
+      expect(comment).to_not be_valid
+    end  
+  end
+
+  describe "post associations" do
+    it "can have many comments" do
+      expect(Post.reflect_on_association(:comments).macro).to eq(:has_many)
+    end
+  end 
+
 end
