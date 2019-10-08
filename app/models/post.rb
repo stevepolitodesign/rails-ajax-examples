f114b049cfed59f1b0d265120fe0daf6ffacd41b
Steve Polito <info@stevepolitodesign.com>
2019-10-07 20:44:02 -0400

    write post model specs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author
   has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    let(:post) { FactoryBot.create(:post) }
+    it "can be created" do
+      expect(post).to be_valid
+    end
+  end
+
+  describe "validations" do
+    let(:post) { FactoryBot.create(:post) }
+    it "must have a title" do
+      post.title = nil
+      expect(post).to_not be_valid
+    end
+    it "must have a body" do
+      post.body = nil
+      expect(post).to_not be_valid
+    end    
+  end
+
+  describe "comment associations" do
+    it "can have many comments" do
+      expect(Post.reflect_on_association(:comments).macro).to eq(:has_many)
+    end
+  end
+
+  describe "author associations" do
+    it "can have many comments" do
+      expect(Post.reflect_on_association(:author).macro).to eq(:belongs_to)
+    end
+  end  
+
 end
