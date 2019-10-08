fba3c9dac62b82004e23a0cd6a4efae7efe37120
Steve Polito <info@stevepolitodesign.com>
2019-10-07 20:27:19 -0400

    create comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_002423) do
+ActiveRecord::Schema.define(version: 2019_10_08_002633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2019_10_08_002423) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -31,5 +39,6 @@ ActiveRecord::Schema.define(version: 2019_10_08_002423) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "authors"
 end
---
+++ spec/factories/comments.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :comment do
+    body { "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede." }
+    post
+  end
+end
