d287d17683cf5fcdf0280ca5012cabe4a6151067
Steve Polito <info@stevepolitodesign.com>
2019-10-08 20:10:45 -0400

    load bootstrap, jquery and popover

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import 'bootstrap'
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +20,4 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
