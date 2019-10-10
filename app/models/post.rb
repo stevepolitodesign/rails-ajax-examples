4d27001247539a7e665233689503f1cca06f1722
Steve Polito <info@stevepolitodesign.com>
2019-10-10 13:59:16 -0400

    write post index spec

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
+
 end
--- spec/features/post_flow_spec.rb
+++ spec/features/post_flow_spec.rb
@@ -5,7 +5,17 @@ RSpec.feature "PostFlows", type: :feature do
   describe "post index page" do
     let!(:post_one) { FactoryBot.create(:post) }
     let!(:post_two) { FactoryBot.create(:post) }
-    pending "displays a list of posts"
+    it "displays a list of posts" do
+      visit posts_path
+      expect(page).to have_content(post_one.title)
+      expect(page).to have_content(post_excerpt(post_one))
+      expect(page).to have_link(post_one.author.formatted_name, href: author_path(post_one.author))
+      expect(page).to have_link("Read More", href: post_path(post_one))
+      expect(page).to have_content(post_two.title)
+      expect(page).to have_content(post_excerpt(post_two))
+      expect(page).to have_link(post_two.author.formatted_name, href: author_path(post_two.author))
+      expect(page).to have_link("Read More", href: post_path(post_two))      
+    end
   end
 
   describe "post show page" do
