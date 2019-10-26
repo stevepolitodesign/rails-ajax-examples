fb50fba98d5cb619c9738ff46515be878767149d
Steve Polito <info@stevepolitodesign.com>
2019-10-26 08:25:24 -0400

    complete styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # TODO add root path
+  root 'posts#index'
   resources :authors
   resources :posts do
     resources :comments
